Fix variance/sd bug with self-adaptive mutation

evolution.py
@@ -131,7 +131,8 @@ def blend_crossover(parent_1, parent_2):
 
 def child_sigma_v4(parent_1, parent_2):
 	"""
-	Sigma calculation for the self-adapting mutation with n step sizes.
+	Sigma calculation for the self-adapting mutation with n step sizes. Uses
+	uniform crossover.
 	"""
 	child_sigma = np.zeros((parent_1.num_genes,))
 	for i in range(parent_1.num_genes):
@@ -139,6 +140,15 @@ def child_sigma_v4(parent_1, parent_2):
 			child_sigma[i] = parent_1.sigma[i]
 		else:
 			child_sigma[i] = parent_2.sigma[i]
+	return child_sigma
+
+def child_sigma_v5(parent_1, parent_2):
+	"""
+	Sigma calculation for the self-adapting mutation with n step sizes. Uses
+	whole arithmetic recombination.
+	"""
+	alpha = np.random.uniform(0, 1.0)
+	child_sigma = alpha * parent_1.sigma + (1-alpha) * parent_2.sigma
 	return child_sigma
 
 

individual.py
@@ -74,9 +74,15 @@ class Individual:
 		# which genes to mutate
 		genes_to_mutate = np.random.uniform(0,1,self.num_genes) <= self.mutation_probability
 		# "mask" actual mutation amounts with genes_to_mutate
-		mutation_amount = genes_to_mutate * np.random.multivariate_normal(
-			mean = np.zeros(self.num_genes),
-			cov = np.diag(self.sigma))
-		
+		#mutation_amount = genes_to_mutate * np.random.multivariate_normal(
+		#	mean = np.zeros(self.num_genes),
+		#	cov = np.diag(self.sigma**2))
+
+		# calculate mutation amounts individually to make sure it goes right
+		# this time
+		mutation_amount = np.zeros((self.num_genes,))
+		for i in range(self.num_genes):
+			mutation_amount[i] = genes_to_mutate[i] * np.random.normal(0, self.sigma[i])
+
 		# apply mutation to genotype
 		self.genotype = weight_limit(self.genotype + mutation_amount)

run_experiment.py
@@ -108,7 +108,7 @@ for run in range(10):
     with open(os.path.join(experiment_name, "run"+str(run)+"_results.csv"), 'w', encoding="UTF-8") as f:
         writer = csv.writer(f)
         writer.writerow(["Generation", "Max", "Mean", "SD", "Stepsize"]) # header
-        print("Generation 0 : Max ({:.3f}) | Mean ({:.3f}) | SD ({:.3f}) | Step size ({:.3f})".format(maximum, mean, sd, avg_step_size)) # logging
+        print("Generation 0 : Max ({:.3f}) | Mean ({:.3f}) | SD ({:.3f}) | Avg step size ({:.3f})".format(maximum, mean, sd, avg_step_size)) # logging
         writer.writerow([0, maximum, mean, sd, avg_step_size])
 
     if progress_visualisation:
@@ -130,10 +130,11 @@ for run in range(10):
         fitness_scores = [individual.fitness for individual in population]
         maximum, mean, sd = most_fit.fitness, np.mean(fitness_scores), np.std(fitness_scores)
         avg_step_size = np.mean([ind.sigma.mean() for ind in population])
+        clipped_prop = np.mean([np.mean((ind.genotype == -1.0) | (ind.genotype == 1.0)) for ind in population])
         with open(os.path.join(experiment_name, "run"+str(run)+"_results.csv"), 'a', encoding="UTF-8") as f:
             writer = csv.writer(f)
             writer.writerow([iteration, maximum, mean, sd, avg_step_size])
-            print("Generation {} : Max ({:.3f}) | Mean ({:.3f}) | SD ({:.3f}) | Step size ({:.3f})".format(iteration, maximum, mean, sd, avg_step_size))
+            print("Generation {} : Max ({:.3f}) | Mean ({:.3f}) | SD ({:.3f}) | Avg step size ({:.3f}) | Clipped prop ({:.2f})".format(iteration, maximum, mean, sd, avg_step_size, clipped_prop))
 
         if progress_visualisation:
             plot_progress(fig, axs, population, fitness_scores, iteration)